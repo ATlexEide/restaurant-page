d82738d7284e062368089e3455ac6194b5e85eee
Alexander <atlexeide@gmail.com>
2024-02-10 16:41:19 +0100

    Add page change logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import './style.css';
 import { printHeader } from './pageload';
 import { loadHome } from './pageload';
-import { pageload } from './pageload';
-import { addEventListeners } from './buttons';
+import { homePage } from './home';
+import { aboutPage } from './about';
+import { menuPage } from './menu';
+
+const home = homePage;
+const about = aboutPage;
+const menu = menuPage;
+let currentPage = home;
 
 printHeader();
 loadHome();
-addEventListeners();
 
+function getClickedButton() {
+    const buttons = document.getElementsByTagName('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute('id', `${buttons[i].innerHTML}`)
+        buttons[i].addEventListener('click', () => {
+            let value = buttons[i].id.toLowerCase()
+            changePage(value); console.log(`click ${buttons[i].id}`); return buttons[i].id;
+        })
+    }
+}
+const container = document.getElementById('container');
+function changePage(value) {
+    value === 'home' ? value = home :
+        value === 'about' ? value = about :
+            value === 'menu' ? value = menu : console.log('oh no')
+    if (value !== currentPage) {
+        container.innerHTML = value;
+        currentPage = value
+    }
+}
 
+getClickedButton()
 console.log('yipp')
